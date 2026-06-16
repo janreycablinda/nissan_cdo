5068d5ef3811ccd2a56beeb7cc7f08e6195b4619
janrey cablinda <45808618+janreycablinda@users.noreply.github.com>
2026-06-17 00:46:22 +0800

    test action

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -106,7 +106,7 @@ export default async function Footer() {
       </div>
 
       <div className="container-x mt-12 border-t border-gray-100 pt-6 text-[11px]">
-        © {new Date().getFullYear()} Nissan Cagayan de Oro - test
+        © {new Date().getFullYear()} Nissan Cagayan de Oro - test 2
       </div>
     </footer>
   );
